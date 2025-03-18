e34988560fcd28b8c2060176ebe2140494d5cc48
bern02 <99220681+bern02@users.noreply.github.com>
2025-03-18 12:31:23 +0100

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -3,7 +3,7 @@ import SimulatorView from '../views/SimulatorView.vue'
 import AboutView from '../views/AboutView.vue'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHistory('#'),
   routes: [
     {
       path: '/',
