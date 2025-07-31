65415829bb1e8aac3ad8cd74731251c9a383d87e
JCMoure <juancarlos.moure@uab.es>
2025-07-31 17:53:27 +0200

    CODE

--- public/rvcat_code.js
+++ public/rvcat_code.js
@@ -3,10 +3,10 @@ const RVCAT_HEADER = function() {
     let prog = currentProgram();
     let res = `import rvcat\n`;
     if (proc !== undefined) {
-      res += `rvcat._processor.load_processor('${currentProcessor()}')\n`
+      res += `rvcat._processor.load('${currentProcessor()}')\n`
     }
     if (prog !== undefined) {
-      res += `rvcat._program.load_program('${currentProgram()}')\n`
+      res += `rvcat._program.load('${currentProgram()}')\n`
       res += `rvcat._scheduler.init_scheduler(iterations=${currentIterations()}, window_size=${currentROBSize()})\n`
     }
     return res;
@@ -21,21 +21,19 @@ const PROG_SHOW_MEMORY       = `rvcat._program.show_memory_trace()`
 const PROG_SHOW_STATIC_PERFORMANCE      = `rvcat._program.show_performance_analysis()`
 const PROG_SHOW_CRITICAL_PATHS_GRAPHVIZ = `rvcat._program.show_graphviz()`
 const GET_AVAIL_PROGRAMS = `import rvcat
-rvcat._program.list_programs_json()
+rvcat.files.list_json(False)
 `
 function addNewProgram(config){
-  let res = `rvcat._program.import_program_json(${config})`;
-  return res;
+  return `rvcat._program.save(${JSON.stringify(config)})`;
 }
 
 // PROCESSOR
 const SHOW_PROCESSOR   = 'rvcat._processor.json()'
 const GET_AVAIL_PROCESSORS = `import rvcat
-rvcat._processor.list_processors_json()
+rvcat.files.list_json(True)
 `
 function addModifiedProcessor(config){
-  let res = `rvcat._processor.import_processor_json(${JSON.stringify(config)})`;
-  return res;
+  return `rvcat._processor.save(${JSON.stringify(config)})`;
 }
 
 // RUN program
