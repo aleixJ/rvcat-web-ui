ac6ee0ee70f2a1853e644f84e1d41ae4269acbd6
JCMoure <juancarlos.moure@uab.es>
2025-08-01 15:49:03 +0200

    Update main.js

--- src/main.js
+++ src/main.js
@@ -3,6 +3,8 @@ import { createPinia } from 'pinia'
 import App             from './App.vue'
 import router          from './router'
 
+import * as Viz from "https://cdn.jsdelivr.net/npm/@viz-js/viz@3.15.0"
+
 const app = createApp(App)
 
 app.use(createPinia())
