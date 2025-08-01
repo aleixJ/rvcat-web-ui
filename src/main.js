9bc1ea838d0880a9886c0896b8091b038544d9e5
JCMoure <juancarlos.moure@uab.es>
2025-08-01 16:30:19 +0200

    VIZ 3.0

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import { createPinia } from 'pinia'
 import App             from './App.vue'
 import router          from './router'
 
-import * as Viz from "https://cdn.jsdelivr.net/npm/@viz-js/viz@3.15.0"
+import { Viz } from 'https://cdn.jsdelivr.net/npm/@viz-js/viz@3.3.1/+esm';
 
 const app = createApp(App)
 
