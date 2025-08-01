07da8129dde8a40127cf4bd6421629ba98fd2173
JCMoure <juancarlos.moure@uab.es>
2025-08-01 14:45:46 +0200

    Add VIZ

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import { createApp }   from 'vue'
 import { createPinia } from 'pinia'
 import App             from './App.vue'
 import router          from './router'
+import * as Viz        from "@viz-js/viz";
 
 const app = createApp(App)
 
