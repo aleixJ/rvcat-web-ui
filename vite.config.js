b9d6cfed819034d872eb5a9313cdff1da35508a8
aleixJ <aleixjb121@gmail.com>
2025-09-06 02:11:28 +0200

    Fix base URL in Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -13,5 +13,5 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
-  base:'https://hpca4se-uab.github.io/rvcat-web-ui/'
+  base: '/rvcat-web-ui/'
 })
